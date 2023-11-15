58cb4e13a0dd222159c0f5f0977da4f704bd38fc
user name <you@example.com>
2023-11-15 16:28:03 +0100

    Initial Commit

---
+++ JavaHelloWorld.java
@@ -0,0 +1,10 @@
+public class JavaHelloWorld {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 36; i++) {
+            System.out.println("Hello World");
+        }
+
+    }
+}
---
+++ PythonHelloWorld.py
@@ -0,0 +1,4 @@
+for x in range (36):
+    print("Hello World")
+
+    
